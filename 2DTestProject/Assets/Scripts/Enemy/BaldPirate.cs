96a3c1afd1c579b2a2db825ea5711f4a3a8daa14
zhanhaor <zhanhaor05@163.com>
2021-07-02 16:13:01 -0700

    Add new enemy
    Blad

---
+++ 2DTestProject/Assets/Scripts/Enemy/BaldPirate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaldPirate : Enemy, IDamageable
+{
+    public void GetHit(float damage)
+    {
+        health -= damage;
+        if(health < 1)
+        {
+            health = 0;
+            isDeadEnemy = true;
+        }
+        enemyAnim.SetTrigger("hit");
+    }
+
+}
